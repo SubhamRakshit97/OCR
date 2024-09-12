de38a62cabf2720ce0b2e6d3d9fadfe88a1adfb9
SubhamRakshit97 <subhamrakshit909@gmail.com>
2024-09-12 19:58:17 +0530

    fixing deployment issues

--- visiting_card_app/visiting_card_app/wsgi.py
+++ visiting_card_app/visiting_card_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visiting_card_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visiting_card_app.visiting_card_app.settings')
 
 application = get_wsgi_application()
