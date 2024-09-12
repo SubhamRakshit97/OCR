56354f1503d3ffef1fe0e700fcda13218b5b796e
SubhamRakshit97 <subhamrakshit909@gmail.com>
2024-09-12 20:07:35 +0530

    fixing deployment issues 2

--- visiting_card_app/visiting_card_app/settings.py
+++ visiting_card_app/visiting_card_app/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cards',
+    'visiting_card_app.cards',
 ]
 
 MIDDLEWARE = [
